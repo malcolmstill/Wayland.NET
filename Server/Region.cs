f993923eeafdef8575e3d5278bdf5f03f3f79b60
Malcolm Still <malcolm.still@gmail.com>
2016-12-31 13:36:23 +0000

    Update to Region

--- Server/Region.cs
+++ Server/Region.cs
@@ -4,29 +4,32 @@ using System.Collections.Generic;
 
 namespace Wayland.Server
 {
-    public class WlRect
-    {
-	public enum Operation
+	public class WlRect
 	{
-	    ADD,
-	    SUBTRACT
-	};
-	
-	public int x { get; set; }
-	public int y { get; set; }
-	public int width { get; set; }
-	public int height { get; set; }
-	public Operation operation { get; set; }
+		public enum Operation
+		{
+			ADD,
+	    	SUBTRACT
+		};
 
-	public bool ContainsPoint(int x, int y)
-	{
-	    return (x >= this.x && x <= (this.x + this.width) && y >= this.y && y <= (this.y + this.height));
-	}
-    }
-    
-    public class WlRegion
-    {
-	public IntPtr regionPtr { get; set; }
-	public List<WlRect> rects = new List<WlRect>();
+		public WlRect(int x, int y, int width, int height, Operation op)
+		{
+			this.x = x;
+			this.y = y;
+			this.width = width;
+			this.height = height;
+			operation = op;
+		}
+
+		public int x { get; set; }
+		public int y { get; set; }
+		public int width { get; set; }
+		public int height { get; set; }
+		public Operation operation { get; set; }
+		
+		public bool ContainsPoint(int x, int y)
+		{
+			return (x >= this.x && x <= (this.x + this.width) && y >= this.y && y <= (this.y + this.height));
+		}
     }
 }
